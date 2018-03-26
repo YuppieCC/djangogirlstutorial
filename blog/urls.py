96bb7bc5da8c0b8d65900cb3b3dfc77b03702204
EacentCheung <eacentcheung@gmail.com>
2018-03-26 09:51:40 +0800

    first complete

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('post/<int:pk>', views.post_detail, name='post_detail'),
+	path('post/new/', views.post_new, name='post_new'),
+	path('post/edit/<int:pk>', views.post_edit, name='post_edit'), 
 ]
