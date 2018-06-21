dd63607c5c68f08535a9ceadeab0384fd5805886
EacentCheung <eacentcheung@gmail.com>
2018-06-21 22:18:15 +0800

    change views

--- blog/views.py
+++ blog/views.py
@@ -4,38 +4,52 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
+
 def index(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request, 'blog/index.html', {'posts': posts})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/index.html', {'posts': posts})
+
 
 def post_detail(request, pk):
-	post = Post.objects.get(pk=pk)
-	return render(request, 'blog/post_detail.html', {'post': post})
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
-	if request.method == "POST":
-		form = PostForm(request.POST)
-		if form.is_valid():
-			post = form.save(commit=False)
-			post.author = request.user
-			post.published_date = timezone.now()
-			post.save()
-			return redirect('post_detail', pk=post.pk)
-	else:
-		form = PostForm()
-	return render(request, 'blog/post_edit.html', {'form': form})
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
 
 def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+'''
+def post_delete(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	if request.method == 'POST':
-		form = PostForm(request.POST, instance=post)
-		if form.is_valid():
-			post = form.save(commit=False)
-			post.author = request.user
-			post.published_date = timezone.now()
-			post.save()
-			return redirect('post_detail', pk=post.pk)
+		post.delete()
+		return redierct('index')
 
-	else:
-		form = PostForm(instance=post)
-	return render(request, 'blog/post_edit.html', {'form': form})
+'''
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-
 urlpatterns = [
     path('', include('blog.urls')),
     path('admin/', admin.site.urls),
